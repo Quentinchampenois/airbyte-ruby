99534d81fc1bd8a33e003865c852d3ef5d117b4a
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2023-09-14 09:58:09 +0200

    Initial commit

---
+++ lib/airbyte_ruby.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "airbyte_ruby/version"
+
+module AirbyteRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/airbyte_ruby_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe AirbyteRuby do
+  it "has a version number" do
+    expect(AirbyteRuby::VERSION).eq "0.1.0"
+  end
+end
