a7aa29c904d521b2f00b0638b24f518909d887fa
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2023-09-14 13:21:43 +0200

    feat: Add specs fixtures

--- Gemfile
+++ Gemfile
@@ -10,3 +10,9 @@ gem "rake", "~> 13.0"
 gem "rspec", "~> 3.0"
 
 gem "rubocop", "~> 1.21"
+
+group :test do
+  gem "rspec", "~> 3.0"
+  gem "vcr", "~> 6.0"
+  gem "webmock", "~> 3.0"
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 require "airbyte_ruby"
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.hook_into :webmock
+end
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
