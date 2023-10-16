aa398acd096b248f5e276d0befa0fa70fa72c59d
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2023-10-16 12:50:34 +0200

    fix: Add specs for AirbyteRuby.api_version

--- spec/airbyte_ruby_spec.rb
+++ spec/airbyte_ruby_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe AirbyteRuby do
   it "has a version number" do
     expect(AirbyteRuby::VERSION).to eq "0.1.0"
   end
+
+  context "when env var is defined" do
+    before do
+      allow(ENV).to receive(:[]).with("AIRBYTE_API_VERSION").and_return("v2")
+    end
+
+    it "returns the env var value" do
+      expect(AirbyteRuby.api_version).to eq "v2"
+    end
+  end
 end
